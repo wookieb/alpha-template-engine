09873119246f42db13ff98625b16fd7708694ee6
Łukasz Kużyński <wookieb>
2017-03-29 20:56:30 +0200

    Added missing jsdoc

--- TemplateEngine.ts
+++ TemplateEngine.ts
@@ -1,4 +1,21 @@
+/**
+ * @template T
+ */
 export interface TemplateEngine<T> {
+    /**
+     * Checks whether given template exists
+     *
+     * @param {string} name
+     * @returns {Promise<boolean>|boolean}
+     */
     hasTemplate(name: string): Promise<boolean>|boolean;
+
+    /**
+     * Returns result of template rendering
+     *
+     * @param {string} name
+     * @param {*} data
+     * @returns {Promise<T>|T}
+     */
     renderTemplate(name: string, data?: any): Promise<T>|T;
 }
